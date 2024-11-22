89cbab739cc4a32875eed863fb410e0805ff6362
Flavia Rea <flavia.rea@cliccaqui.org>
2024-11-22 18:52:34 +0100

    Add files via upload
    modified ft_strnstr

--- part1/ft_strnstr.c
+++ part1/ft_strnstr.c
@@ -11,13 +11,14 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 /*	ft_strnstr:
 **	Locates the first occurrence of the substring 'to_find' within the string 'str',
-**	looking only within the first 'len' characters.
+**	looking only within the first 'n' characters.
 **	Returns a pointer to the beginning of the located substring or NULL if 'to_find' is not found.
 */
-char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+char	*ft_strnstr(const char *str, const char *to_find, size_t n)
 {
 	size_t	i;
 	size_t	j;
@@ -25,12 +26,12 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 	i = 0;
 	if (!to_find[0] || to_find == NULL)
 		return ((char *)str);
-	if (len == 0)
+	if (n == 0)
 		return (NULL);
-	while (i < len && str[i])
+	while (i < n && str[i])
 	{
 		j = 0;
-		while ((i + j) < len && to_find[j] && str[i + j] == to_find[j])
+		while (to_find[j] && str[i + j] == to_find[j])
 		{
 			if (to_find[j + 1] == '\0')
 				return ((char *)(str + i));
@@ -40,3 +41,15 @@ char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 	}
 	return (NULL);
 }
+
+/*
+int main(int argc, char **argv)
+{
+	char *str = "Hello world lower more";
+	char *to_find = "lower";
+	
+	printf("%s\n", ft_strnstr(str, to_find, 20));
+	
+	return 0;
+}
+*/
